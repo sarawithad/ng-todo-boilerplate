29a97e9cb8959c24a4a7a952e12a5ad2dc723cb6
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-02-24 10:43:18 -0600

    completed crud app

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,47 +1,23 @@
 "use strict";
 
-app.controller("ItemListCtrl", function ($scope) {
+app.controller("ItemListCtrl", function ($scope, ItemStorage, SearchTermData, AuthFactory){
 
-    $scope.items = [
-        {
-        id: 0,
-        task: "mow the lawn",
-        isCompleted: false,
-        dueDate: "12/5/17",
-        assignedTo: "Greg",
-        location: "Joe's house",
-        urgency: "low",
-        dependencies: "sunshine, clippers, hat, water, headphones"
-        },
-        {
-        id: 1,
-        task: "grade quizzes",
-        isCompleted: false,
-        dueDate: "12/5/15",
-        assignedTo: "Christina",
-        location: "NSS",
-        urgency: "high",
-        dependencies: "wifi, tissues, vodka"
-        },
-        {
-        id: 2,
-        task: "take a nap",
-        isCompleted: false,
-        dueDate: "5/21/16",
-        assignedTo: "Joe",
-        location: "Porch of lakefront cabin",
-        urgency: "medium",
-        dependencies: "hammock, silence"
-        }
-    ];
+    $scope.searchText = SearchTermData;
+    let user = AuthFactory.getUser();
+
+    ItemStorage.getItemList(user)
+    .then(function(itemCollection){
+        $scope.items = itemCollection;
+    });
 
     $scope.itemDelete = function(itemId){
         console.log("delete this item", itemId);
-        for (var i = 0; i < $scope.items.length; i++){
-            if ($scope.items[i].id === itemId) {
-                $scope.items.splice(i,1);
-            }
-        }     
+        ItemStorage.deleteItem(itemId)
+        .then(function(response){
+            ItemStorage.getItemList(user).then(function(itemCollection){
+                $scope.items = itemCollection;
+            });
+        });   
     };
    
 });
